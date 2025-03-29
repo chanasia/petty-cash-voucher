b5d8dba41baa70ec75278ce59539c560ebda26ef
Chananuwat Chaysri <asiavip2546@gmail.com>
2025-03-30 05:28:00 +0700

    fix: fixed volume readonly to writable.

--- sql/init.sh
+++ sql/init.sh
@@ -2,13 +2,15 @@
 
 if [ -f /sql_backup/sql_data_volume.tar.gz ]; then
   echo "Restoring volume from backup..."
-  mkdir -p /tmp_restore
-  tar -xzf /sql_backup/sql_data_volume.tar.gz -C /tmp_restore
-  cp -a /tmp_restore/. /var/opt/mssql/
-  rm -rf /tmp_restore
+
+  mkdir -p /var/opt/mssql/tmp_restore
+  tar -xzf /sql_backup/sql_data_volume.tar.gz -C /var/opt/mssql/tmp_restore
+  cp -a /var/opt/mssql/tmp_restore/. /var/opt/mssql/
+  rm -rf /var/opt/mssql/tmp_restore
   echo "Volume restore completed."
 else
   echo "No backup file found, using empty volume."
 fi
 
+# Start SQL Server
 /opt/mssql/bin/sqlservr
